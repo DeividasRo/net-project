50aee5e8a4ee1d94191aefe8dca5f99b52eea54f
DeividasRo <slenderiss@gmail.com>
2023-10-26 01:32:49 +0300

    Ready button partly implemented

--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -5,6 +5,8 @@ public class PlayerNetwork : NetworkBehaviour
 {
     private NetworkVariable<int> _randomObjectCount = new NetworkVariable<int>(50, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     private NetworkVariable<Vector2> _maxSpawnPositions = new NetworkVariable<Vector2>(new Vector2(3, 3), NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    public NetworkVariable<bool> isReady = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
@@ -21,14 +23,38 @@ public class PlayerNetwork : NetworkBehaviour
 
     private void OnClientConnected(ulong clientId)
     {
-        if (NetworkManager.Singleton.ConnectedClientsList.Count == NetworkManager.Singleton.GetComponent<Relay>().maxConnections)
-            PrepareGame();
+        Debug.Log($"Client {clientId} connected");
     }
 
-    private void PrepareGame()
+    public void ChangeReadyState()
     {
-        if (IsServer && IsLocalPlayer)
+        if (!IsOwner) return;
+        isReady.Value = !isReady.Value;
+        Invoke("CheckReadinessServerRpc", 2f / NetworkManager.Singleton.NetworkTickSystem.TickRate);
+    }
+
+    [ServerRpc]
+    public void CheckReadinessServerRpc()
+    {
+        foreach (NetworkClient networkClient in NetworkManager.Singleton.ConnectedClientsList)
         {
+            Debug.Log(networkClient.ClientId);
+            Debug.Log(networkClient.PlayerObject.GetComponent<PlayerNetwork>().isReady.Value);
+            if (!networkClient.PlayerObject.GetComponent<PlayerNetwork>().isReady.Value)
+                return;
+        }
+        if (NetworkManager.Singleton.ConnectedClientsList.Count != NetworkManager.Singleton.GetComponent<Relay>().maxConnections)
+        {
+            return;
+        }
+        PrepareGame();
+    }
+
+    public void PrepareGame()
+    {
+        if (IsServer)
+        {
+            Debug.Log("Preparing the game...");
             _randomObjectCount.Value = Random.Range(50, 1000);
             Invoke("StartGameClientRpc", 3);
         }
@@ -41,5 +67,7 @@ public class PlayerNetwork : NetworkBehaviour
         Debug.Log("Game started!");
         GameManager.Instance.StartGame(_randomObjectCount.Value, _maxSpawnPositions.Value);
     }
+
+
 
 }
--- Assets/Scripts/UIGameManager.cs
+++ Assets/Scripts/UIGameManager.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
 using TMPro;
 using Unity.Netcode;
+using UnityEngine.UI;
 
 public class UIGameManager : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text _codeText;
+    [SerializeField]
+    private Button _readyButton;
 
     private void Start()
     {
         _codeText.text = GameObject.Find("NetworkManager").GetComponent<Relay>().joinCode;
+    }
+
+    public void OnReadyButtonClicked()
+    {
+        PlayerNetwork playerNetwork = NetworkManager.Singleton.LocalClient.PlayerObject.GetComponent<PlayerNetwork>();
+        playerNetwork.isReady.OnValueChanged += OnReadyValueChanged;
+        playerNetwork.ChangeReadyState();
+    }
+
+    private void OnReadyValueChanged(bool oldVal, bool newVal)
+    {
+        if (newVal)
+        {
+            _readyButton.GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            _readyButton.GetComponent<Image>().color = Color.white;
+        }
     }
 }
